8d525fad950f0c8b51a2c3d661b10f39a6e3db97
Lucas Oliveira França <lucasfranca65@gmail.com>
2023-10-02 21:08:11 -0300

    ajustes 02-10-2023

--- aplication.py
+++ aplication.py
@@ -31,28 +31,71 @@ class Functions():
         self.connectDB()
         print("Conectado ao banco de dados comsucesso")
         # Criar Tabelas
-        self.cursor.executescript("""
+        self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS clientes(
                 matricula INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 nome CHAR(40) NOT NULL,
                 email CHRAR(40),
-                telefone INTEGER(13) NOT NULL                
-            );
-            CREATE TABLE IF NOT EXISTS veiculos(
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+                telefone INTEGER(13) NOT NULL ,
                 montadora CHAR(10),
                 modelo CHAR(40) NOT NULL,
                 ano INTEGER(4),
-                placa CHAR(7) NOT NULL                
+                placa CHAR(7) NOT NULL               
             );                
         """)
         self.conn.commit()
         print('Banco de Dados criado')
         self.desconnectDB()
 
+    def capturaVariaveis(self):
+        self.nome = self.en_name.get()
+        self.email = self.en_email.get()
+        self.phone = self.en_phone.get()
+        self.montadora = self.en_montadora.get()
+        self.modelo = self.en_modelo.get()
+        self.ano = self.en_ano.get()
+        self.placa = self.en_placa.get()
+
+    def add_cliente(self):
+
+        self.capturaVariaveis()
+        self.connectDB()
+        self.cursor.execute(""" INSERT INTO clientes (nome, email, telefone, montadora, modelo, ano, placa)
+                VALUES (?,?,?,?,?,?,?)""", (self.nome, self.email, self.phone, self.montadora, self.modelo, self.ano, self.placa))
+        self.conn.commit()
+        self.desconnectDB()
+        self.selec_list()
+        self.limpar()
+
+    def selec_list(self):
+        self.listaCli.delete(*self.listaCli.get_children())
+        self.connectDB()
+        lista = self.cursor.execute(
+            """ SELECT  matricula, nome, telefone, email,montadora, modelo, ano, placa FROM clientes ORDER BY nome ASC; """)
+
+        for i in lista:
+            self.listaCli.insert("", END, values=i)
+
+        self.desconnectDB()
+
+    def onDoubleClick(self):
+        self.limpar()
+        self.listaCli.selection()
+
+        for n in self.listaCli.selection():
+            col1, col2, col3, col4, col5, col6, col7, col8 = self.listaCLi.item(
+                n, 'values')
+            self.en_matricula.insert(END, col1)
+            self.en_name.insert(END, col2)
+            self.en_phone.insert(END, col3)
+            self.en_email.insert(END, col4)
+            self.en_montadora.insert(END, col5)
+            self.en_modelo.insert(END, col6)
+            self.en_ano.insert(END, col7)
+            self.en_placa.insert(END, col8)
+
+
 # Classe daAplicação
-
-
 class Aplication(Functions):
     def __init__(self):
         self.root = root
@@ -62,6 +105,7 @@ class Aplication(Functions):
         self.buttons_form_frame_1()
         self.lista_frame2()
         self.mountTables()
+        self.selec_list()
         root.mainloop()
 
 # Função da tela principal
@@ -156,7 +200,7 @@ class Aplication(Functions):
         self.bt_buscar.place(relx=0.25, rely=0.1, relwidth=0.1, relheight=0.1)
         # Botão Novo
         self.bt_novo = Button(self.frame1, text="Novo",
-                              font=('verdana', 8, 'bold'))
+                              font=('verdana', 8, 'bold'), command=self.add_cliente)
         self.bt_novo.place(relx=0.66, rely=0.1, relwidth=0.1, relheight=0.1)
         # Botão Editar
         self.bt_edit = Button(self.frame1, text="Editar",
@@ -170,20 +214,26 @@ class Aplication(Functions):
 # Lista cadastrados frame 2
     def lista_frame2(self):
         self.listaCli = ttk.Treeview(self.frame2, height=3, columns=(
-            "col1", "col2", "col3", "col4", "col5"))
+            "col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8"))
         self.listaCli.heading("#0", text="")
         self.listaCli.heading("#1", text="Matricula")
         self.listaCli.heading("#2", text="Nome")
-        self.listaCli.heading("#3", text="Modelo")
-        self.listaCli.heading("#4", text="Placa")
-        self.listaCli.heading("#5", text="Telefone")
+        self.listaCli.heading("#3", text="Telefone")
+        self.listaCli.heading("#4", text="Email")
+        self.listaCli.heading("#5", text="Montadora")
+        self.listaCli.heading("#6", text="Modelo")
+        self.listaCli.heading("#7", text="Ano")
+        self.listaCli.heading("#8", text="placa")
         # Definindo tamanho das colunas
         self.listaCli.column('#0', width=1)
-        self.listaCli.column('#1', width=50)
-        self.listaCli.column('#2', width=200)
-        self.listaCli.column('#3', width=100)
-        self.listaCli.column('#4', width=100)
+        self.listaCli.column('#1', width=30)
+        self.listaCli.column('#2', width=100)
+        self.listaCli.column('#3', width=50)
+        self.listaCli.column('#4', width=120)
         self.listaCli.column('#5', width=50)
+        self.listaCli.column('#6', width=70)
+        self.listaCli.column('#7', width=30)
+        self.listaCli.column('#8', width=50)
         # Posicionando a tabela
         self.listaCli.place(relx=0.02, rely=0.1, relwidth=0.94, relheight=0.85)
         # Configuração da ScrollBar
